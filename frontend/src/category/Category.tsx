d7c7f1d003a3fec145a2a5ba56f683bdee298b60
alexismreyes <alexis_mreyes@hotmail.com>
2024-06-29 16:20:20 -0600

    feat: :sparkles: Now its possible to render categories and products in the frontend

--- frontend/src/category/Category.tsx
+++ frontend/src/category/Category.tsx
@@ -1,17 +1,37 @@
 import axios from "axios"
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 
 const Category = () =>{
 
+    const [categories,setCategories] = useState([])
+    
     useEffect(()=>{
         axios.get("http://localhost:8080/category/")
         .then((response)=>{
-            console.log("Respuesta del backend->",response);
+            //console.log("Respuesta del backend->",response);
+            setCategories(response.data)
         })            
     },[])
 
+    if(!categories || categories.length===0) return <h1>FETCHING DATA...</h1>
+
     return (
-        <h3>Revisar consola</h3>
+        <table>
+            <thead>
+                <tr>
+                    <th>Category ID</th><th>Name</th>
+                </tr>
+            </thead>    
+                <tbody>
+                    { categories.map((category)=>(
+                        <tr key={category.id}>
+                        <td>{category.id}</td>   
+                        <td>{category.name}</td>   
+                    </tr>
+                    ))}                    
+                </tbody>
+            
+        </table>
         
 
     )
--- frontend/src/products/Products.tsx
+++ frontend/src/products/Products.tsx
@@ -1,6 +1,37 @@
+import axios from "axios"
+import { useEffect, useState } from "react"
+
 const Products = () =>{
+    const [products,setProducts] = useState([])
+    
+    useEffect(()=>{
+        axios.get("http://localhost:8080/products/")
+        .then((response)=>{            
+            setProducts(response.data)
+        })            
+    },[])
+
+    if(!products || products.length===0) return <h1>FETCHING DATA...</h1>
+
     return (
-        <h1>Soy products</h1>
+        <table>
+            <thead>
+                <tr>
+                    <th>Product ID</th><th>Name</th>
+                </tr>
+            </thead>    
+                <tbody>
+                    { products.map((product)=>(
+                        <tr key={product.id}>
+                        <td>{product.id}</td>   
+                        <td>{product.name}</td>   
+                    </tr>
+                    ))}                    
+                </tbody>
+            
+        </table>
+        
+
     )
 }
 export default Products
